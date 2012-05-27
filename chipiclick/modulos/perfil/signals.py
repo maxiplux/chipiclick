0c1a7b5331a297e01691ddf348306ec80d86aa11
Juan Francisco Mosquera <maxiplux@gmail.com>
2012-05-26 21:29:53 -0500

    conceptuao

---
+++ chipiclick/modulos/perfil/signals.py
@@ -0,0 +1,13 @@
+__author__ = 'juan'
+
+from  chipiclick.modulos.perfil.models import  Perfil
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+    #Make sure we create a FacebookProfile when creating a User
+def create_facebook_profile(sender, instance, created, **kwargs):
+        if created:
+            Perfil.objects.create(user=instance)
+
+post_save.connect(create_facebook_profile, sender=User)
+
