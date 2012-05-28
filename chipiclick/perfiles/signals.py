58d5f7f7567c21d5f8d5e402196d3ae9c40f33f3
juan <maxiplux@gmail.com>
2012-05-28 14:32:37 -0500

    32r2332

--- chipiclick/perfiles/signals.py
+++ chipiclick/perfiles/signals.py
@@ -1,6 +1,6 @@
 __author__ = 'juan'
 
-from  perfiles.models import  Perfil
+from  chipiclick.perfiles.models import  Perfil
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
